a33eea00b104fd1bc39509dfc012335cfd2b8b92
ldd <leonardoldd@gmail.com>
2018-06-09 21:29:22 -0400

    style(js): prettify js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,37 @@
-var gulp = require('gulp'),
-    gulpIf = require('gulp-if'),
-    gulpCopy = require('gulp-copy');
+var gulp = require("gulp"),
+  gulpIf = require("gulp-if"),
+  gulpCopy = require("gulp-copy");
 
-var useref = require('gulp-useref'),
-    uglify = require('gulp-uglify'),
-    sourcemaps = require('gulp-sourcemaps'),
-    lazypipe = require('lazypipe');
+var useref = require("gulp-useref"),
+  uglify = require("gulp-uglify"),
+  sourcemaps = require("gulp-sourcemaps"),
+  lazypipe = require("lazypipe");
 
-var webserver = require('gulp-webserver');
+var webserver = require("gulp-webserver");
 
-gulp.task('serve', function() {
-    gulp.src('.')
-        .pipe(webserver({}));
+gulp.task("serve", function() {
+  gulp.src(".").pipe(webserver({}));
 });
 
-gulp.task('useref', function(){
-    return gulp.src('index.html')
-        .pipe(useref({}, lazypipe().pipe(sourcemaps.init, { loadMaps: true })))
-            .pipe(gulpIf('js/*.js', uglify()))
-            .pipe(gulpIf('js/*.js', sourcemaps.write('.')))
-        .pipe(gulp.dest('dist'));
+gulp.task("useref", function() {
+  return gulp
+    .src("index.html")
+    .pipe(
+      useref(
+        {},
+        lazypipe().pipe(
+          sourcemaps.init,
+          { loadMaps: true }
+        )
+      )
+    )
+    .pipe(gulpIf("js/*.js", uglify()))
+    .pipe(gulpIf("js/*.js", sourcemaps.write(".")))
+    .pipe(gulp.dest("dist"));
 });
 
-gulp.task('copyImages', function(){
-    return gulp.src(['images/**/*.png'])
-        .pipe(gulpCopy('dist'));
+gulp.task("copyImages", function() {
+  return gulp.src(["images/**/*.png"]).pipe(gulpCopy("dist"));
 });
 
-gulp.task('default', ['copyImages','useref']);
+gulp.task("default", ["copyImages", "useref"]);
--- js/jsonLoader.js
+++ js/jsonLoader.js
@@ -1,16 +1,22 @@
-var techTree = (function(api){
-    api.createTreeFromJSON = function(config){
-        d3.json(config.offsetsFileName, function(error, jsonOffsets){
-            if (error) {return console.warn(error);}
-            d3.json(config.dataFileName, function(error, json) {
-                if (error) {return console.warn(error);}
-                d3.json(config.settingsFileName, function(error, settings) {
-                    if (error) {return console.warn(error);}
-                    techTree.createTree(json.nodes, settings, jsonOffsets);
-                });
-            });
+var techTree = (function(api) {
+  api.createTreeFromJSON = function(config) {
+    d3.json(config.offsetsFileName, function(error, jsonOffsets) {
+      if (error) {
+        return console.warn(error);
+      }
+      d3.json(config.dataFileName, function(error, json) {
+        if (error) {
+          return console.warn(error);
+        }
+        d3.json(config.settingsFileName, function(error, settings) {
+          if (error) {
+            return console.warn(error);
+          }
+          techTree.createTree(json.nodes, settings, jsonOffsets);
         });
-    };
+      });
+    });
+  };
 
-    return api;
-}(techTree || {}));
+  return api;
+})(techTree || {});
